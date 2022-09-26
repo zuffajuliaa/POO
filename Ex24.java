3d0798a7ff066d978df94f669676b53768280cb2
zuffajuliaa <100146435+zuffajuliaa@users.noreply.github.com>
2022-09-25 22:37:57 -0300

    Update Ex24.java

--- Ex24.java
+++ Ex24.java
@@ -3,31 +3,28 @@ import java.util.Scanner;
 import java.io.IOException;
 public class Ex24 {
 	
-
-		public static void main(String[] args) throws IOException {
-			Scanner ler = new Scanner(System.in);
-			
-			
-			String n, e;
-			char s;
-			int a;
-			
-			System.out.print("Digite o seu nome: ");
-			n = ler.next();
-			System.out.print("Digite o seu sexo(F/M):");
-		    s = (char)System.in.read();
-			System.out.print("Digite o seu estado civil(CASADA(O)/SOLTEIRA(O): ");
-			e = ler.next();
-			if(s == 'F')
+	public static void main(String[] args) throws IOException {
+		Scanner ler = new Scanner(System.in);
+		
+		String n, e;
+		char s;
+		int a;
+		
+		System.out.print("Digite o seu nome: ");
+		n = ler.next();
+		System.out.print("Digite o seu sexo(F/M):");
+	    s = (char)System.in.read();
+		System.out.print("Digite o seu estado civil(CASADA(O)/SOLTEIRA(O): ");
+		e = ler.next();
+		if(s == 'F')
+		{
+			if( e.equals("CASADA") )
 			{
-				if(e == "CASADA")
-				{
-					System.out.printf("Digite o tempo de casada em anos: ");
-					a = ler.nextInt();
-				}
+				System.out.printf("Digite o tempo de casada em anos: ");
+				a = ler.nextInt();
 			}
-
 		}
-	}
 
+	}
+}
 
